bff57bf2d358a439b2b43bae6821fed654d21494
syahirahsohami <syahieyrahsohami@gmail.com>
2024-07-01 10:41:51 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'locationApp',
+  webDir: 'www'
+};
+
+export default config;
